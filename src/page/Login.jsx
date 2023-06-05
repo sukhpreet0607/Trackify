700510e8b5f1438806e0d1fb82e26ffe845b6fea
Sukhpreet Singh <sukhpreetsingh06072001@gmail.com>
2023-06-05 17:50:17 +0530

    NaN

---
+++ src/page/Login.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+// import styled from 'styled-components';
+// import PropTypes from 'prop-types';
+
+
+// const propTypes = {};
+
+// const defaultProps = {};
+
+
+const Login = () => {
+    return <>
+        <p>hello</p>
+    </>;
+}
+
+// login.propTypes = propTypes;
+// login.defaultProps = defaultProps;
+
+export default Login;
